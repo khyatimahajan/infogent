cf14129b2a65d8b449ffced6f96c4d44324cb484
khyatimahajan <khyati.mahajan@servicenow.com>
2025-02-14 06:43:27 +0000

    updating models for frames

--- direct-api-driven/run_aggregation_frames.sh
+++ direct-api-driven/run_aggregation_frames.sh
@@ -7,13 +7,12 @@
 mkdir -p results
 
 # Run FanOutQA aggregation
-python run_fanoutqa.py \
-    --navigator_model gpt-4o-mini \
-    --aggregator_model gpt-4o-mini \
-    --extractor_model gpt-4o-mini \
+python run_fanoutqa_wiki.py \
+    --navigator_model meta-llama/Llama-3.1-70B-Instruct \
+    --aggregator_model meta-llama/Llama-3.1-70B-Instruct \
+    --extractor_model meta-llama/Llama-3.1-70B-Instruct \
     --chat_deployment ${CHAT_DEPLOYMENT} \
     --embedding_deployment ${EMBEDDING_DEPLOYMENT} \
-    --api_version ${API_VERSION} \
     --inp_path data/test_frames_w_ids.json \
     --out_path results/frames_agg_2.json \
     --log_path results/frames_agg_2.log 
